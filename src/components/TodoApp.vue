447e771a98697811bba17a024c5bca783747d0d9
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-07-08 06:46:51 +0100

    test: adding a new todo

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-        <div v-for="todo in todos" :key="todo.id" data-test="todo">{{ todo.text }}</div>
-    </div>
+  <div>
+    <div v-for="todo in todos" :key="todo.id" data-test="todo">{{ todo.text }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 interface Todo {
-    id: number
-    text: string
-    completed: boolean
+  id: number
+  text: string
+  completed: boolean
 }
 
 const todos: Todo[] = [
-    {
-        id: 1,
-        text: 'Learn Vue.js 3',
-        completed: true
-    }
+  {
+    id: 1,
+    text: 'Learn Vue.js 3',
+    completed: true
+  }
 ]
 
 console.log(todos)
--- src/components/__tests__/TodoApp.spec.ts
+++ src/components/__tests__/TodoApp.spec.ts
@@ -8,3 +8,13 @@ test('creates a todo', () => {
   const todo = wrapper.get('[data-test="todo"]')
   expect(todo.text()).toBe('Learn Vue.js 3')
 })
+
+test('creates a todo', () => {
+  const wrapper = mount(TodoApp)
+  expect(wrapper.findAll('[data-test]="todo"')).toHaveLength(1)
+
+  wrapper.get('[data-test="new-todo"]').setValue('New todo')
+  wrapper.get('[data-test="form"]').trigger('submit')
+
+  expect(wrapper.findAll('[data-test]="todo"')).toHaveLength(2)
+})
