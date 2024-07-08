f0f4aca7b5319839e3213e6824778bc100d7a8a3
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-07-08 07:12:01 +0100

    test completing a todo

--- src/components/__tests__/TodoApp.spec.ts
+++ src/components/__tests__/TodoApp.spec.ts
@@ -18,3 +18,11 @@ test('adds a todo', async () => {
 
   expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
 })
+
+test('mark todo as completed', async () => {
+  const wrapper = mount(TodoApp)
+
+  await wrapper.get('[data-test="todo-checkbox"]').setValue(true)
+
+  expect(wrapper.get('[data-test="todo"]').classes()).toContain('completed')
+})
